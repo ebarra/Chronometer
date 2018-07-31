c6298f3a4a764fadd1385be7abb06e67b970ac2f
Álvaro Alonso <aalonsog@dit.upm.es>
2018-07-31 11:27:03 +0200

    Updated styles

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { CronometroComponent } from './chronometer/chronometer.component';
+import { ChronometerComponent } from './chronometer/chronometer.component';
 
 
 @NgModule({
